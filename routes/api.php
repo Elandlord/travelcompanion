557a21da56871846f0b7b6ef494cfb072337773f
Ruben Klink <rubenjklink@gmail.com>
2017-01-13 19:36:06 +0100

    Add api routes and controllers

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+
+Route::resource('users', 'UsersController');
+Route::resource('users.routes', 'RoutesController');
+Route::resource('users.routes.hotels', 'HotelsController');
