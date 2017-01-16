20fc8ca87971856571ff3ff4cf9640d8f8020bee
Eric Landheer <Eric Landheer>
2017-01-16 15:02:16 +0100

    logout
    and several lay-out bugfixes

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -1,16 +1,15 @@
 <nav class="navbar navbar-default navbar-fixed-top bg-main">
     <div class="container">
         <div class="navbar-header">
-            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
             <a class="navbar-brand" href="/"><img src='images/banner-logo.png' style='width:175px;'/></a>
         </div>
-        <center>
-            <div class="navbar-collapse collapse" id="navbar-main">
-                <form class="navbar-form navbar-left" role="search">
+        <div class="navbar-collapse collapse bg-main" id="navbar-main">
+                <form class="navbar-form navbar-left hidden-sm hidden-xs" role="search">
                     <div class="form-group">
                         <input type="text" style="width:250px;" class="form-control" name="username" placeholder="Where do you want to go?">
                     </div>
@@ -18,13 +17,20 @@
                 </form>
 
                 @if (Auth::check())
-                    <form class="navbar-form navbar-right space-outside-up-sm" role="search">
-                        <div class="form-group">
-                            <p class='text-color-light font-md'>{{ Auth::user()->name }} <i class="fa fa-caret-down" aria-hidden="true"></i></p>
-                        </div>
-                    </form>
+                    <div class="dropdown navbar-right space-outside-up-sm sm-space-outside-xl xs-space-outside-xl">
+                      <button class="btn hover-darken-accent transition-normal dropdown-toggle bg-accent  text-color-light" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        {{ Auth::user()->name }}
+                        <span class="caret"></span>
+                      </button>
+                      <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                        <li><a href="#">MyTrip</a></li>
+                        <li><a href="#">Hotels</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="logout">Logout</a></li>
+                      </ul>
+                    </div>
                 @else
-                    <form class="navbar-form navbar-right" role="form" method="POST" action="{{ url('/login') }}">
+                    <form class="navbar-form navbar-right border-accent sm-space-outside-xl xs-space-outside-xl" role="form" method="POST" action="{{ url('/login') }}">
                          {{ csrf_field() }}
                         <a class='text-color-accent text-hover-light transition-normal space-outside-right-sm' href='register'>No account?</a>
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
@@ -50,6 +56,5 @@
                 @endif
 
             </div>
-        </center>
     </div>
 </nav>
